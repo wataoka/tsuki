2c20ecea7785cb30a71af65f275169ce19da702e
Koki Wataoka <contact@wataoka.com>
2018-06-14 12:52:46 +0900

    first

---
+++ main.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*- #
+
+import sys
+import numpy as np
+from PIL import Image
+
+from utils import load_tsuki_matrix, preprocess, index2tsuki
+
+
+
+if __name__ == "__main__":
+    tsuki_matrixs = load_tsuki_matrix()
+
+    img = preprocess("./apple.png")
+
+    tsuki_list = []
+
+    for i in range(int(np.shape(img)[0]/4)):
+        for j in range(int(np.shape(img)[1]/4)):
+            row = 4*i
+            col = 4*j
+            max = -10000
+            max_tk = 0
+
+            for n, tk in enumerate(tsuki_matrixs):
+                hadamard = np.multiply(img[row:row+4, col:col+4], tk)
+
+                if max < hadamard.sum():
+                    max_index = n
+                    max = hadamard.sum()
+
+            tsuki_list.append(index2tsuki(max_index))
+        tsuki_list.append('\n')
+
+
+
+    for i in tsuki_list:
+        sys.stdout.write(i)
+
---
+++ utils.py
@@ -0,0 +1,95 @@
+# -*- coding: utf-8 -*- #
+
+import emoji
+import numpy as np
+from PIL import Image
+
+def load_tsuki_matrix():
+    """
+    月行列を返す関数
+
+    Args:
+        無し
+    Return:
+        月行列8個, (numpyの行列のリスト)
+    """
+    tsuki_0000 = np.matrix([[-1, -1, -1, -1],
+                      [-1, -1, -1, -1],
+                      [-1, -1, -1, -1],
+                      [-1, -1, -1, -1]])
+
+    tsuki_0001 = np.matrix([[-1, -1, -1, 1],
+                      [-1, -1, -1, 1],
+                      [-1, -1, -1, 1],
+                      [-1, -1, -1, 1]])
+
+    tsuki_0011 = np.matrix([[-1, -1, 1, 1],
+                      [-1, -1, 1, 1],
+                      [-1, -1, 1, 1],
+                      [-1, -1, 1, 1]])
+
+    tsuki_0111 = np.matrix([[-1, 1, 1, 1],
+                      [-1, 1, 1, 1],
+                      [-1, 1, 1, 1],
+                      [-1, 1, 1, 1]])
+
+    tsuki_1000 = np.matrix([[1, -1, -1, -1],
+                      [1, -1, -1, -1],
+                      [1, -1, -1, -1],
+                      [1, -1, -1, -1]])
+
+    tsuki_1100 = np.matrix([[1, 1, -1, -1],
+                           [1, 1, -1, -1],
+                           [1, 1, -1, -1],
+                           [1, 1, -1, -1]])
+
+    tsuki_1110 = np.matrix([[1, 1, 1, -1],
+                           [1, 1, 1, -1],
+                           [1, 1, 1, -1],
+                           [1, 1, 1, -1]])
+
+    tsuki_1111 = np.matrix([[1, 1, 1, 1],
+                           [1, 1, 1, 1],
+                           [1, 1, 1, 1],
+                           [1, 1, 1, 1]])
+
+    return [tsuki_0000, tsuki_0001, tsuki_0011, tsuki_0111,
+            tsuki_1000, tsuki_1100, tsuki_1110, tsuki_1111,]
+
+
+def index2tsuki(index):
+    if index==0:
+        return emoji.emojize(':new_moon:', use_aliases=True)
+    if index==1:
+        return emoji.emojize(':waxing_crescent_moon:', use_aliases=True)
+    if index==2:
+        return emoji.emojize(':first_quarter_moon:', use_aliases=True)
+    if index==3:
+        return emoji.emojize(':waxing_gibbous_moon:', use_aliases=True)
+    if index==4:
+        return emoji.emojize(':waning_crescent_moon:', use_aliases=True)
+    if index==5:
+        return emoji.emojize(':last_quarter_moon:', use_aliases=True)
+    if index==6:
+        return emoji.emojize(':waning_gibbous_moon:', use_aliases=True)
+    else:
+        return emoji.emojize(':full_moon:', use_aliases=True)
+
+
+def preprocess(img_file):
+
+
+    img = Image.open(img_file)
+    img = img.convert('L')
+
+
+    width = 160
+    height = int(width*(img.height/img.width))
+    height -= height%4
+
+    img = img.resize((width, height))
+    img = np.matrix(img)
+    img = ((np.abs(img-255.) / 128.) - 1.0) * (-1)
+
+
+    return img
