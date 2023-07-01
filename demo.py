cf5d3b573f36ba21b251350b02e070a97bfc3842
wataoka <wataoka.koki@gmail.com>
2023-07-01 14:22:47 +0900

    Change: col of demo

--- demo.py
+++ demo.py
@@ -7,7 +7,7 @@ import tsuki
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--path", default="./images/apple.png", type=str)
-    parser.add_argument("--col", default=50, type=int)
+    parser.add_argument("--col", default=30, type=int)
     args = parser.parse_args()
     return args
 
